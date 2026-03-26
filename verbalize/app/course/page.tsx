deeb913fb0bfe996d30c5a32bd20988f297acd4c
Rupesh Senthil <rupeshsenthil17@gmail.com>
2026-03-25 20:52:44 -0500

    hardcoded student code AI

--- verbalize/app/course/page.tsx
+++ verbalize/app/course/page.tsx
@@ -33,7 +33,7 @@ export default function Course() {
         body: JSON.stringify({
           course_name: courseName,
           section_num: sectionNum,
-          student_info: studentInfo,
+          
         }),
       });
 
